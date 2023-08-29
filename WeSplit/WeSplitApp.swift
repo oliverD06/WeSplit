ae328117665828096036e8f6068d279314d866ae
Oliver Delaney <oliver.delaney@stu.barren.kyschools.us>
2023-08-29 10:46:27 -0500

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Oliver Delaney on 8/29/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
